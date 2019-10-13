c5916673bba9e19ccf7cf271aa597d7b4088ba53
sirisjo <siri.sjo@gmail.com>
2019-10-13 16:41:40 -0500

    total price and unit price now works!

--- js/shopping_cart.js
+++ js/shopping_cart.js
@@ -5,6 +5,8 @@ item_prices["product2"] = 1;
 item_prices["product3"] = 1;
 item_prices["product4"] = 1;
 
+var currentTotal = parseInt(document.getElementById("total").innerHTML);
+
 function getItemPrice() {
   var itemPrice = 0;
   var theForm = document.forms["cartform"];
@@ -19,15 +21,24 @@ function getQuantity() {
   return quantity.value;
 }
 
+function setUnitPrice() {
+  document.getElementById("unitPrice").innerHTML = parseInt(
+    getItemPrice() * getQuantity()
+  );
+}
+
 function calculateTotal() {
-  var totalPrice = getItemPrice() * getQuantity();
-  document.getElementById("total").innerHTML = currentTotal + totalPrice;
+  var totalPrice = parseInt(getItemPrice() * getQuantity());
+  currentTotal += totalPrice;
+  document.getElementById("total").innerHTML = currentTotal;
 }
 
 if (document.getElementById("addToCart")) {
-  var currentTotal = document.getElementById("total").innerHTML;
-
   document
     .getElementById("addToCart")
     .addEventListener("click", calculateTotal);
+}
+
+if (document.getElementById("units")) {
+  document.getElementById("units").addEventListener("change", setUnitPrice);
 }
