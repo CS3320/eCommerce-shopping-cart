f35a0eefb994edefeb502db7e6d08a273cd21a1f
bwb63 <bwb63@txstate.edu>
2019-10-13 19:18:26 -0500

    Made sure that code works on all servers

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
-$(document).ready(function () {
-    
-    var dropdown = $('#states');
-    
+$(document).ready(function() {
+    var dropdown = $("#states");
+
     dropdown.empty();
-    
+
     dropdown.append('<option selected="true" disabled> Choose State </option>');
-    dropdown.prop('selectedIndex',0);
-    
+    dropdown.prop("selectedIndex", 0);
+
+    const host = location.hostname;
     const port = location.port;
-    const url = 'http://127.0.0.1:' + port + '/js/states.json';
-    
+    const url = "http://" + host + ":" + port + "/js/states.json";
+    console.log(url);
     //populate dropdown with list of states
     $.getJSON(url, function(data) {
         $.each(data, function(key, entry) {
-            dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.name));
-        })
+            dropdown.append(
+                $("<option></option>")
+                    .attr("value", entry.abbreviation)
+                    .text(entry.name)
+            );
+        });
     });
 });
