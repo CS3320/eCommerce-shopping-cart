03729fcae52e2a3c81a2edcdbadbab1aef04537f
bwb63 <bwb63@txstate.edu>
2019-10-13 19:08:40 -0500

    JavaScript + jQuery populate states
    Added js and jq to populate the list of states for users to choose from. Note that users code will have to be altered in the future as the state option will have to be retrieved as user data from the database later.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-
-  // jQuery methods go here...
-
+$(document).ready(function () {
+    
+    var dropdown = $('#states');
+    
+    dropdown.empty();
+    
+    dropdown.append('<option selected="true" disabled> Choose State </option>');
+    dropdown.prop('selectedIndex',0);
+    
+    const port = location.port;
+    const url = 'http://127.0.0.1:' + port + '/js/states.json';
+    
+    //populate dropdown with list of states
+    $.getJSON(url, function(data) {
+        $.each(data, function(key, entry) {
+            dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.name));
+        })
+    });
 });
