387721da0452ca8f9852c804a24802544f1f25b2
sirisjo <siri.sjo@gmail.com>
2019-11-07 22:18:27 -0600

    states dropdown loads from database

--- js/script.js
+++ js/script.js
@@ -1,89 +1,88 @@
 $(document).ready(function() {
-    const host = location.hostname;
-    const port = location.port;
-    let url = "http://" + host + ":" + port;
-    
-    
-    
-    
-        let dropdown = $("#states");
+  const host = location.hostname;
+  const port = location.port;
+  let url = "http://" + host + ":" + port;
 
-        dropdown.empty();
+  // let dropdown = $("#states");
 
-        dropdown.append('<option selected="true" disabled> Choose State </option>');
-        dropdown.prop("selectedIndex", 0);
-        
-        let urlCurrent = "http://" + host + ":" + port + "/js/states.json";
-        //populate dropdown with list of states
-        $.getJSON(urlCurrent, function(data) {
-            $.each(data, function(key, entry) {
-                dropdown.append(
-                    $("<option></option>")
-                        .attr("value", entry.abbreviation)
-                        .text(entry.name)
-                );
-            });
-        });
-    
-    
-    
-        
-        //checkout section begin
-        
+  // dropdown.empty();
 
-        window.onload = loadCurrentValues();
+  // dropdown.append('<option selected="true" disabled> Choose State </option>');
+  // dropdown.prop("selectedIndex", 0);
 
-        //we have unit price, total price, and units as local storage variables
-        function loadCurrentValues() {
-            setItemCount();
-            setShippingHandling();
-            setTotalBeforeTax();
-            setEstimatedTax();
-            setOrderTotal();
-        }
-        function setItemCount(){
-            if(document.getElementById("itemCount")){
-                document.getElementById("itemCount").innerHTML = "Items: " + parseInt(localStorage.getItem("itemCount"));
-            }else{
-               document.getElementById("itemCount").innerHTML = " $0"; 
-            }
-        }
+  // let urlCurrent = "http://" + host + ":" + port + "/js/states.json";
+  // //populate dropdown with list of states
+  // $.getJSON(urlCurrent, function(data) {
+  //     $.each(data, function(key, entry) {
+  //         dropdown.append(
+  //             $("<option></option>")
+  //                 .attr("value", entry.abbreviation)
+  //                 .text(entry.name)
+  //         );
+  //     });
+  // });
 
+  //checkout section begin
 
-        function setShippingHandling(){
-            //default value currently
-            if(document.getElementById("shippingAndHandling")){
-                document.getElementById("shippingAndHandling").innerHTML = "Shipping & Handling: $" + parseFloat(localStorage.getItem("totalPrice")*0.03);
-            }else{
-               document.getElementById("shippingAndHandling").innerHTML = "Shipping & Handling: $0"; 
-            }
+  window.onload = loadCurrentValues();
 
-        }
-        function setTotalBeforeTax(){
-            if(!(localStorage.getItem("totalPrice") === null)){
-                document.getElementById("totalBeforeTax").innerHTML = "Total before tax: $" + localStorage.getItem("totalPrice") + ".00";
-            }else{
-               document.getElementById("estimatedTax").innerHTML = "Total before tax: $0.00"; 
-            }
-        }
-        function setEstimatedTax(){
+  //we have unit price, total price, and units as local storage variables
+  function loadCurrentValues() {
+    setItemCount();
+    setShippingHandling();
+    setTotalBeforeTax();
+    setEstimatedTax();
+    setOrderTotal();
+  }
+  function setItemCount() {
+    if (document.getElementById("itemCount")) {
+      document.getElementById("itemCount").innerHTML =
+        "Items: " + parseInt(localStorage.getItem("itemCount"));
+    } else {
+      document.getElementById("itemCount").innerHTML = " $0";
+    }
+  }
 
-            if(!(localStorage.getItem("totalPrice") === null)){
-                let tax = parseFloat(localStorage.getItem("totalPrice")) * 0.08;
-                document.getElementById("estimatedTax").innerHTML = "Estimated Tax: $" + tax.toFixed(2)
-            }else{
-               document.getElementById("estimatedTax").innerHTML = "Estimated Tax: $0"; 
-            }
-        }
-        function setOrderTotal(){
-            let totalWithTaxAndShipping = parseFloat(localStorage.getItem("totalPrice"))*0.03 +    parseFloat(localStorage.getItem("totalPrice")) + 0.08*parseFloat(localStorage.getItem("totalPrice")).toFixed(2);
-            
-            document.getElementById("orderTotal").innerHTML = "Order Total: $" + totalWithTaxAndShipping;
-            let withShipping = totalWithTaxAndShipping*.03;
-            localStorage.setItem('shippingAndHandling', withShipping);
-        }
+  function setShippingHandling() {
+    //default value currently
+    if (document.getElementById("shippingAndHandling")) {
+      document.getElementById("shippingAndHandling").innerHTML =
+        "Shipping & Handling: $" +
+        parseFloat(localStorage.getItem("totalPrice") * 0.03);
+    } else {
+      document.getElementById("shippingAndHandling").innerHTML =
+        "Shipping & Handling: $0";
+    }
+  }
+  function setTotalBeforeTax() {
+    if (!(localStorage.getItem("totalPrice") === null)) {
+      document.getElementById("totalBeforeTax").innerHTML =
+        "Total before tax: $" + localStorage.getItem("totalPrice") + ".00";
+    } else {
+      document.getElementById("estimatedTax").innerHTML =
+        "Total before tax: $0.00";
+    }
+  }
+  function setEstimatedTax() {
+    if (!(localStorage.getItem("totalPrice") === null)) {
+      let tax = parseFloat(localStorage.getItem("totalPrice")) * 0.08;
+      document.getElementById("estimatedTax").innerHTML =
+        "Estimated Tax: $" + tax.toFixed(2);
+    } else {
+      document.getElementById("estimatedTax").innerHTML = "Estimated Tax: $0";
+    }
+  }
+  function setOrderTotal() {
+    let totalWithTaxAndShipping =
+      parseFloat(localStorage.getItem("totalPrice")) * 0.03 +
+      parseFloat(localStorage.getItem("totalPrice")) +
+      0.08 * parseFloat(localStorage.getItem("totalPrice")).toFixed(2);
 
-    //check
+    document.getElementById("orderTotal").innerHTML =
+      "Order Total: $" + totalWithTaxAndShipping;
+    let withShipping = totalWithTaxAndShipping * 0.03;
+    localStorage.setItem("shippingAndHandling", withShipping);
+  }
+
+  //check
 });
-
-
---
+++ php/getStates.php
@@ -0,0 +1,30 @@
+<?php
+$servername = 'localhost';
+$user = 'root';
+$pass = '89.8_CapVB1';
+$db = 'cs3320';
+   
+$con = new mysqli($servername, $user, $pass, $db) or die("Unable to connect");
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+$sql="SELECT * FROM states";
+//where stateCode = 
+//('$_GET[stateCode]')";
+//echo $sql;
+//execute the INSERT
+$result = mysqli_query($con, $sql);
+if (!$result){
+    printf("Error: %s\n", mysqli_error($con));
+    exit();
+}
+    $returnStates = "[";
+    while($row = mysqli_fetch_array($result)) {
+        $returnStates = $returnStates . "{state_abbr: \"" . $row['state_abbr'] . "\"," . "state_name: \"" . $row['state_name'] . "\"},";
+    }
+    $returnStates = $returnStates ."]";
+    
+echo $returnStates;
+//return $returnStates;
+mysqli_close($con);
+?>
